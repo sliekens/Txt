d5d5521b8ee5850637e5a8c3075ddb914d381116
Steven Liekens <steven.liekens@gmail.com>
2014-12-12 15:22:48 +0100

    Adding support for 'DQUOTE' tokens (rfc5234)

---
+++ src/Text/src/Core/DQuoteLexer.cs
@@ -0,0 +1,35 @@
+﻿namespace Text.Core
+{
+    public class DQuoteLexer : Lexer<DQuoteToken>
+    {
+        public DQuoteLexer(ITextScanner scanner)
+            : base(scanner)
+        {
+        }
+
+        public override DQuoteToken Read()
+        {
+            var context = this.Scanner.GetContext();
+            DQuoteToken token;
+            if (this.TryRead(out token))
+            {
+                return token;
+            }
+
+            throw new SyntaxErrorException("Expected DQUOTE", context);
+        }
+
+        public override bool TryRead(out DQuoteToken token)
+        {
+            token = default(DQuoteToken);
+            var context = this.Scanner.GetContext();
+            if (this.Scanner.TryMatch('\"'))
+            {
+                token = new DQuoteToken(context);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Text/src/Core/DQuoteToken.cs
@@ -0,0 +1,10 @@
+﻿namespace Text.Core
+{
+    public class DQuoteToken : Token
+    {
+        public DQuoteToken(ITextContext context)
+            : base("\"", context)
+        {
+        }
+    }
+}
