8dbe0653a6d74a25511d05cf6e9d264d5d460693
Steven Liekens <steven.liekens@gmail.com>
2014-12-15 11:56:39 +0100

    Fixing programming errors in 'CtlLexer' + refactoring 'CtlLexer' and 'CtlToken'

--- src/Text/src/Core/CtlLexer.cs
+++ src/Text/src/Core/CtlLexer.cs
@@ -16,23 +16,32 @@
                 return token;
             }
 
-            throw new SyntaxErrorException("Expected CTL", context);
+            throw new SyntaxErrorException("Expected 'CTL'", context);
         }
 
         public override bool TryRead(out CtlToken token)
         {
-            token = default(CtlToken);
             var context = this.Scanner.GetContext();
-            for (int i = 0x01; i <= 0x7F; i++)
+
+            // %x00-1F
+            for (int i = 0x00; i <= 0x1F; i++)
             {
                 var c = (char)i;
                 if (this.Scanner.TryMatch(c))
                 {
-                    token = new CtlToken(char.ToString(c), context);
+                    token = new CtlToken(c, context);
                     return true;
                 }
             }
 
+            // %x7F
+            if (this.Scanner.TryMatch((char)0x7F))
+            {
+                token = new CtlToken((char)0x7F, context);
+                return true;
+            }
+
+            token = default(CtlToken);
             return false;
         }
     }
--- src/Text/src/Core/CtlToken.cs
+++ src/Text/src/Core/CtlToken.cs
@@ -1,10 +1,13 @@
-﻿namespace Text.Core
+﻿using System.Diagnostics.Contracts;
+
+namespace Text.Core
 {
     public class CtlToken : Token
     {
-        public CtlToken(string data, ITextContext context)
+        public CtlToken(char data, ITextContext context)
             : base(data, context)
         {
+            Contract.Requires((data >= 0x00 && data <= 0x1F) || data == 0x7F);
         }
     }
 }
