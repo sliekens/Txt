5b51fcf177eef3be47d132309c13d8f92cc29120
Steven Liekens <steven.liekens@gmail.com>
2014-12-15 11:29:56 +0100

    Refactoring 'CharLexer' and 'CharToken'

--- src/Text/src/Core/CharLexer.cs
+++ src/Text/src/Core/CharLexer.cs
@@ -16,23 +16,23 @@
                 return token;
             }
 
-            throw new SyntaxErrorException("Expected CHAR", context);
+            throw new SyntaxErrorException("Expected 'CHAR'", context);
         }
 
         public override bool TryRead(out CharToken token)
         {
-            token = default(CharToken);
             var context = this.Scanner.GetContext();
             for (var i = 0x01; i <= 0x7F; i++)
             {
-                var character = (char) i;
-                if (this.Scanner.TryMatch(character))
+                var c = (char) i;
+                if (this.Scanner.TryMatch(c))
                 {
-                    token = new CharToken(char.ToString(character), context);
+                    token = new CharToken(c, context);
                     return true;
                 }
             }
 
+            token = default(CharToken);
             return false;
         }
     }
--- src/Text/src/Core/CharToken.cs
+++ src/Text/src/Core/CharToken.cs
@@ -1,11 +1,14 @@
-﻿namespace Text.Core
+﻿using System.Diagnostics.Contracts;
+
+namespace Text.Core
 {
     /// <summary>\u0001 - \u007F</summary>
     public class CharToken : Token
     {
-        public CharToken(string data, ITextContext context)
+        public CharToken(char data, ITextContext context)
             : base(data, context)
         {
+            Contract.Requires(data >= 0x01 && data <= 0x7F);
         }
     }
 }
