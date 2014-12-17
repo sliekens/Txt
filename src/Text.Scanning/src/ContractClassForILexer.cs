15633375d8009aa01d2e3f408fd3ab7c8e676cc3
Steven Liekens <steven.liekens@gmail.com>
2014-12-17 16:58:52 +0100

    Improving contracts for 'ILexer'

--- src/Text.Scanning/src/ContractClassForILexer.cs
+++ src/Text.Scanning/src/ContractClassForILexer.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Diagnostics.Contracts;
-
-namespace Text.Scanning
+﻿namespace Text.Scanning
 {
+    using System;
+    using System.Diagnostics.Contracts;
+
     [ContractClassFor(typeof(ILexer<>))]
     internal abstract class ContractClassForILexer<TToken> : ILexer<TToken> where TToken : Token
     {
+        public void PutBack(TToken token)
+        {
+            Contract.Requires(token != null);
+            throw new NotImplementedException();
+        }
+
         public TToken Read()
         {
             Contract.Ensures(Contract.Result<TToken>() != null);
@@ -14,12 +20,8 @@ namespace Text.Scanning
 
         public bool TryRead(out TToken token)
         {
-            Contract.Ensures(Contract.ValueAtReturn(out token) != null && Contract.Result<bool>() == true || Contract.Result<bool>() == false);
-            throw new NotImplementedException();
-        }
-
-        public void PutBack(TToken token)
-        {
+            Contract.Ensures(Contract.Result<bool>() && (Contract.ValueAtReturn(out token) != null) ||
+                             Contract.Result<bool>() == false);
             throw new NotImplementedException();
         }
     }
