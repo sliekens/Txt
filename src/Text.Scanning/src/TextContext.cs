f2fff4490df955cd2ebc841b3c749b9e82d85cd0
Steven Liekens <steven.liekens@gmail.com>
2014-12-17 12:12:19 +0100

    Adding xml doc comments to 'TextContext'

--- src/Text.Scanning/src/TextContext.cs
+++ src/Text.Scanning/src/TextContext.cs
@@ -1,10 +1,14 @@
 ﻿namespace Text.Scanning
 {
+    /// <summary>Represents an immutable implementation of the <see cref="ITextContext" /> interface</summary>
     public struct TextContext : ITextContext
     {
         private readonly int offset;
 
-        public TextContext(int offset) : this()
+        /// <summary>Initializes a new instance of the <see cref="TextContext" /> struct with a specified offset.</summary>
+        /// <param name="offset">The position, relative to the beginning of the data source.</param>
+        public TextContext(int offset)
+            : this()
         {
             this.offset = offset;
         }
@@ -14,7 +18,7 @@
         {
             get
             {
-                return offset;
+                return this.offset;
             }
         }
     }
