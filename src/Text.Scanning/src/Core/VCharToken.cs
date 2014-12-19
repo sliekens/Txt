89ed2bc93e73f5cc79f1688453ec9332f785f072
Steven Liekens <steven.liekens@gmail.com>
2014-12-19 10:54:50 +0100

    Adding xml doc comments

--- src/Text.Scanning/src/Core/VCharToken.cs
+++ src/Text.Scanning/src/Core/VCharToken.cs
@@ -2,8 +2,15 @@
 {
     using System.Diagnostics.Contracts;
 
+    /// <summary>Represents a visible (printing) char. Unicode: U+0021 - U+007E.</summary>
     public class VCharToken : Token
     {
+        /// <summary>
+        /// Creates a new instance of the <see cref="T:Text.Scanning.Core.VCharToken" /> class with a specified character
+        /// and context.
+        /// </summary>
+        /// <param name="data">The VCHAR character.</param>
+        /// <param name="context">The object that describes the context in which the text appears.</param>
         public VCharToken(char data, ITextContext context)
             : base(data, context)
         {
