79bc05ab0d83eec58805d8276da7acf5f3129a7b
Steven Liekens <steven.liekens@gmail.com>
2015-02-14 21:43:39 +0100

    Disabling post-conditions for ITextScanner that are difficult to prove in a multithreaded environment

--- src/Text.Scanning/src/ContractClassForITextScanner.cs
+++ src/Text.Scanning/src/ContractClassForITextScanner.cs
@@ -33,19 +33,19 @@
 
         public void PutBack(char c)
         {
-            Contract.Ensures(Contract.OldValue(this.Offset) == this.Offset + 1 || this.Offset == 0);
+            //Contract.Ensures(Contract.OldValue(this.Offset) == this.Offset + 1 || this.Offset == 0);
             throw new NotImplementedException();
         }
 
         public bool Read()
         {
-            Contract.Ensures(Contract.OldValue(this.Offset) == this.Offset - 1 || this.EndOfInput);
+            //Contract.Ensures(Contract.OldValue(this.Offset) == this.Offset - 1 || this.EndOfInput);
             throw new NotImplementedException();
         }
 
         public bool TryMatch(char c)
         {
-            Contract.Ensures(c != this.NextCharacter || Contract.OldValue(this.Offset) == this.Offset - 1 || this.EndOfInput);
+            //Contract.Ensures(c != this.NextCharacter || Contract.OldValue(this.Offset) == this.Offset - 1 || this.EndOfInput);
             throw new NotImplementedException();
         }
     }
