7a860fad1e2104451a62b809a0fd4e95349e7af2
Steven Liekens <steven.liekens@gmail.com>
2015-02-15 21:00:14 +0100

    Adding placeholders for automatic assembly versioning

--- src/Text.Scanning/src/Properties/AssemblyInfo.cs
+++ src/Text.Scanning/src/Properties/AssemblyInfo.cs
@@ -26,4 +26,6 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
