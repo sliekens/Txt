aec8ad7e0c8fbd1471529c83ad41efbc88ac7b53
Steven Liekens <steven.liekens@gmail.com>
2015-02-20 23:40:10 +0100

    Adding class 'TokenMutex<T1, T2>'
    The token mutex class is useful for parsing grammar rules that contain alternatives, of the form: Rule1 / Rule2
    
    Example: foo / bar
    will accept <foo> or <bar>.
    
    The TokenMutex can represent an empty string,  <foo> or <bar>, but not <foobar>.

---
+++ src/Text.Scanning/src/TokenMutex`2.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Text.Scanning
+{
+    public class TokenMutex<T1, T2>
+        where T1 : Token
+        where T2 : Token
+    {
+        private readonly T1 token1;
+        private readonly T2 token2;
+
+        public TokenMutex(T1 token)
+        {
+            Contract.Requires(token != null);
+            token1 = token;
+        }
+
+        public TokenMutex(T2 token)
+        {
+            Contract.Requires(token != null);
+            token2 = token;
+        }
+
+        public Token Token
+        {
+            get { return token1 as Token ?? token2; }
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(token1 == null || token2 == null);
+        }
+    }
+}
