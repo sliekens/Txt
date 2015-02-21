11396d814ef9322437d777ce8dd43b7cfa6735b6
Steven Liekens <steven.liekens@gmail.com>
2015-02-21 16:32:27 +0100

    Documenting 'Alternative<T1,T2>'

--- src/Text.Scanning/src/Alternative{T1,T2}.cs
+++ src/Text.Scanning/src/Alternative{T1,T2}.cs
@@ -1,44 +1,45 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Alternative`2.cs" company="Steven Liekens">
+// <copyright file="Alternative{T1,T2}.cs" company="Steven Liekens">
 //   The MIT License (MIT)
 // </copyright>
 // <summary>
-//   
+//   Represents a choice of two alternative elements.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 namespace Text.Scanning
 {
+    using System.Diagnostics.CodeAnalysis;
     using System.Diagnostics.Contracts;
 
-    /// <summary>TODO </summary>
-    /// <typeparam name="T1"></typeparam>
-    /// <typeparam name="T2"></typeparam>
+    /// <summary>Represents a choice of two alternative elements.</summary>
+    /// <typeparam name="T1">The type of the first alternative element.</typeparam>
+    /// <typeparam name="T2">The type of the second alternative element.</typeparam>
     public class Alternative<T1, T2>
         where T1 : Element where T2 : Element
     {
-        /// <summary>TODO </summary>
+        /// <summary>The first alternative element, or a default value.</summary>
         private readonly T1 element1;
 
-        /// <summary>TODO </summary>
+        /// <summary>The second alternative element, or a default value.</summary>
         private readonly T2 element2;
 
-        /// <summary>TODO </summary>
-        /// <param name="element">TODO </param>
+        /// <summary>Initializes a new instance of the <see cref="Alternative{T1,T2}"/> class.</summary>
+        /// <param name="element">The alternative element.</param>
         public Alternative(T1 element)
         {
             Contract.Requires(element != null);
             this.element1 = element;
         }
 
-        /// <summary>TODO </summary>
-        /// <param name="element">TODO </param>
+        /// <summary>Initializes a new instance of the <see cref="Alternative{T1,T2}"/> class.</summary>
+        /// <param name="element">The alternative element.</param>
         public Alternative(T2 element)
         {
             Contract.Requires(element != null);
             this.element2 = element;
         }
 
-        /// <summary>TODO </summary>
+        /// <summary>Gets the alternative element.</summary>
         public Element Element
         {
             get
@@ -48,17 +49,13 @@ namespace Text.Scanning
                     return this.element1;
                 }
 
-                if (this.element2 != default(T2))
-                {
-                    return this.element2;
-                }
-
-                return default(Element);
+                return this.element2;
             }
         }
 
-        /// <summary>TODO </summary>
         [ContractInvariantMethod]
+        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", 
+            Justification = "Reviewed. Suppression is OK here.")]
         private void ObjectInvariant()
         {
             Contract.Invariant(this.element1 == null || this.element2 == null);
