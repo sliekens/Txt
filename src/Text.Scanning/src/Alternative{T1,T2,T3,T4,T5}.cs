cfdc521fdf92876e7763a290d914539a7ae596e2
Steven Liekens <steven.liekens@gmail.com>
2015-02-21 16:48:58 +0100

    Adding class 'Alternative' for 5 alternatives

--- src/Text.Scanning/src/Alternative{T1,T2,T3,T4,T5}.cs
+++ src/Text.Scanning/src/Alternative{T1,T2,T3,T4,T5}.cs
@@ -1,27 +1,28 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Alternative{T1,T2,T3,T4} .cs" company="Steven Liekens">
+// <copyright file="Alternative{T1,T2,T3,T4,T5}.cs" company="Steven Liekens">
 //   The MIT License (MIT)
 // </copyright>
 // <summary>
-//   Represents a choice of four alternative elements.
+//   Represents a choice of five alternative elements.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 namespace Text.Scanning
 {
     using System.Diagnostics.Contracts;
 
-    /// <summary>Represents a choice of four alternative elements.</summary>
+    /// <summary>Represents a choice of five alternative elements.</summary>
     /// <typeparam name="T1">The type of the first alternative element.</typeparam>
     /// <typeparam name="T2">The type of the second alternative element.</typeparam>
     /// <typeparam name="T3">The type of the third alternative element.</typeparam>
     /// <typeparam name="T4">The type of the fourth alternative element.</typeparam>
-    public class Alternative<T1, T2, T3, T4> : Element
-        where T1 : Element where T2 : Element where T3 : Element where T4 : Element
+    /// <typeparam name="T5">The type of the fifth alternative element.</typeparam>
+    public class Alternative<T1, T2, T3, T4, T5> : Element
+        where T1 : Element where T2 : Element where T3 : Element where T4 : Element where T5 : Element
     {
         /// <summary>The alternative element.</summary>
         private readonly Element element;
 
-        /// <summary>Initializes a new instance of the <see cref="Alternative{T1,T2,T3,T4}"/> class with a specified alternative.</summary>
+        /// <summary>Initializes a new instance of the <see cref="Alternative{T1,T2,T3,T4,T5}"/> class with a specified alternative.</summary>
         /// <param name="element">The alternative element.</param>
         /// <param name="context">The object that describes the context in which the text appears.</param>
         public Alternative(T1 element, ITextContext context)
@@ -31,7 +32,7 @@ namespace Text.Scanning
             this.element = element;
         }
 
-        /// <summary>Initializes a new instance of the <see cref="Alternative{T1,T2,T3,T4}"/> class with a specified alternative.</summary>
+        /// <summary>Initializes a new instance of the <see cref="Alternative{T1,T2,T3,T4,T5}"/> class with a specified alternative.</summary>
         /// <param name="element">The alternative element.</param>
         /// <param name="context">The object that describes the context in which the text appears.</param>
         public Alternative(T2 element, ITextContext context)
@@ -41,7 +42,7 @@ namespace Text.Scanning
             this.element = element;
         }
 
-        /// <summary>Initializes a new instance of the <see cref="Alternative{T1,T2,T3,T4}"/> class with a specified alternative.</summary>
+        /// <summary>Initializes a new instance of the <see cref="Alternative{T1,T2,T3,T4,T5}"/> class with a specified alternative.</summary>
         /// <param name="element">The alternative element.</param>
         /// <param name="context">The object that describes the context in which the text appears.</param>
         public Alternative(T3 element, ITextContext context)
@@ -51,10 +52,20 @@ namespace Text.Scanning
             this.element = element;
         }
 
-        /// <summary>Initializes a new instance of the <see cref="Alternative{T1,T2,T3,T4}"/> class with a specified alternative.</summary>
+        /// <summary>Initializes a new instance of the <see cref="Alternative{T1,T2,T3,T4,T5}"/> class with a specified alternative.</summary>
         /// <param name="element">The alternative element.</param>
         /// <param name="context">The object that describes the context in which the text appears.</param>
         public Alternative(T4 element, ITextContext context)
+            : base(element.Data, context)
+        {
+            Contract.Requires(element != null);
+            this.element = element;
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Alternative{T1,T2,T3,T4,T5}"/> class with a specified alternative.</summary>
+        /// <param name="element">The alternative element.</param>
+        /// <param name="context">The object that describes the context in which the text appears.</param>
+        public Alternative(T5 element, ITextContext context)
             : base(element.Data, context)
         {
             Contract.Requires(element != null);
