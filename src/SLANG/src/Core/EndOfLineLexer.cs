4091d8fbadb009c697ed0cc5b463b9182f2a4621
Steven Liekens <steven.liekens@gmail.com>
2015-03-15 17:24:08 +0100

    Changing 'EndOfLineLexer' to derive from 'SequenceLexer'

--- src/SLANG/src/Core/EndOfLineLexer.cs
+++ src/SLANG/src/Core/EndOfLineLexer.cs
@@ -10,57 +10,48 @@ namespace SLANG.Core
     using System.Diagnostics;
     using System.Diagnostics.Contracts;
 
-    public class EndOfLineLexer : Lexer<EndOfLine>
+    public class EndOfLineLexer : SequenceLexer<EndOfLine, CarriageReturn, LineFeed>
     {
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private readonly ILexer<CarriageReturn> crLexer;
+        private readonly ILexer<CarriageReturn> carriageReturnLexer;
 
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private readonly ILexer<LineFeed> lfLexer;
+        private readonly ILexer<LineFeed> lineFeedLexer;
 
         public EndOfLineLexer()
             : this(new CarriageReturnLexer(), new LineFeedLexer())
         {
         }
 
-        public EndOfLineLexer(ILexer<CarriageReturn> crLexer, ILexer<LineFeed> lfLexer)
+        public EndOfLineLexer(ILexer<CarriageReturn> carriageReturnLexer, ILexer<LineFeed> lineFeedLexer)
             : base("CRLF")
         {
-            Contract.Requires(crLexer != null);
-            Contract.Requires(lfLexer != null);
-            this.crLexer = crLexer;
-            this.lfLexer = lfLexer;
+            Contract.Requires(carriageReturnLexer != null);
+            Contract.Requires(lineFeedLexer != null);
+            this.carriageReturnLexer = carriageReturnLexer;
+            this.lineFeedLexer = lineFeedLexer;
         }
 
-        /// <inheritdoc />
-        public override bool TryRead(ITextScanner scanner, out EndOfLine element)
+        protected override EndOfLine CreateInstance(CarriageReturn element1, LineFeed element2, ITextContext context)
         {
-            var context = scanner.GetContext();
-            CarriageReturn carriageReturn;
-            if (scanner.EndOfInput || !this.crLexer.TryRead(scanner, out carriageReturn))
-            {
-                element = default(EndOfLine);
-                return false;
-            }
+            return new EndOfLine(element1, element2, context);
+        }
 
-            LineFeed lineFeed;
-            if (scanner.EndOfInput || !this.lfLexer.TryRead(scanner, out lineFeed))
-            {
-                scanner.PutBack(carriageReturn.Data);
-                element = default(EndOfLine);
-                return false;
-            }
+        protected override bool TryRead(ITextScanner scanner, out CarriageReturn element)
+        {
+            return this.carriageReturnLexer.TryRead(scanner, out element);
+        }
 
-            Contract.Assume(lineFeed.Offset == carriageReturn.Offset + 1);
-            element = new EndOfLine(carriageReturn, lineFeed, context);
-            return true;
+        protected override bool TryRead(ITextScanner scanner, out LineFeed element)
+        {
+            return this.lineFeedLexer.TryRead(scanner, out element);
         }
 
         [ContractInvariantMethod]
         private void ObjectInvariant()
         {
-            Contract.Invariant(this.crLexer != null);
-            Contract.Invariant(this.lfLexer != null);
+            Contract.Invariant(this.carriageReturnLexer != null);
+            Contract.Invariant(this.lineFeedLexer != null);
         }
     }
 }
