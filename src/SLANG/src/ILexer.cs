b4da003aeef4b68d3e8e2bbecd5442d7743a49c3
Steven Liekens <steven.liekens@gmail.com>
2015-03-15 13:23:33 +0100

    Improving xml doc comments for 'ILexer'

--- src/SLANG/src/ILexer.cs
+++ src/SLANG/src/ILexer.cs
@@ -21,16 +21,19 @@ namespace SLANG
         string RuleName { get; }
 
         /// <summary>Reads the next element.</summary>
-        /// <param name="scanner"></param>
-        /// <exception cref="T:SLANG.SyntaxErrorException"></exception>
+        /// <param name="scanner">The scanner object that provides text symbols as well as contextual information about the text source.</param>
+        /// <exception cref="T:System.InvalidOperationException">The given scanner object is not initialized.</exception>
+        /// <exception cref="T:SLANG.SyntaxErrorException">The scanner returned an unexpected symbol, or unexpectedly reached the end of the text source.</exception>
         /// <returns>The next available element.</returns>
         TElement Read(ITextScanner scanner);
 
-        /// <summary>Reads the next element. A return value indicates whether a element was available.</summary>
-        /// <param name="scanner"></param>
+        /// <summary>Attempts to read the next element. A return value indicates whether the element was available.</summary>
+        /// <param name="scanner">The scanner object that provides text symbols as well as contextual information about the text source.</param>
         /// <param name="element">When this method returns, contains the next available element, or a <c>null</c> reference, depending
         /// on whether the return value indicates success.</param>
+        /// <exception cref="T:System.InvalidOperationException">The given scanner object is not initialized.</exception>
+        /// <exception cref="T:System.ObjectDisposedException">The given text scanner is closed.</exception>
         /// <returns><c>true</c> to indicate success; otherwise, <c>false</c>.</returns>
         bool TryRead(ITextScanner scanner, out TElement element);
     }
-}
+}   
