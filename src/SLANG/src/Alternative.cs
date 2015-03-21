5dbf32f151da76fc0889db7a2a725bc81c3c2063
Steven Liekens <steven.liekens@gmail.com>
2015-03-21 12:54:20 +0100

    Adding a class 'Alternative' for ranges of alternative elements
    Grammar syntax: %c##-## (RFC 5234)

---
+++ src/SLANG/src/Alternative.cs
@@ -0,0 +1,20 @@
+﻿namespace SLANG
+{
+    using System.Diagnostics.Contracts;
+
+    /// <summary>Represents a choice of a range of alternative elements.</summary>
+    public class Alternative : Element
+    {
+        /// <summary>Initializes a new instance of the <see cref="Alternative"/> class with a specified alternative.</summary>
+        /// <param name="data">The alternative element.</param>
+        /// <param name="lowerBound">The lower bound of the range of alternatives.</param>
+        /// <param name="upperBound">The upper bound of the range of alternatives.</param>
+        /// <param name="context">The object that describes the context in which the text appears.</param>
+        public Alternative(char data, char lowerBound, char upperBound, ITextContext context)
+            : base(data, context)
+        {
+            Contract.Requires(data >= lowerBound && data <= upperBound);
+            Contract.Requires(context != null);
+        }
+    }
+}
