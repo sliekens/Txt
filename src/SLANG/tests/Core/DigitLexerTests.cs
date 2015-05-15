d95a03057e8c49265829eabf08ec582bd661f547
Steven Liekens <steven.liekens@gmail.com>
2015-05-15 18:07:50 +0200

    Adding DigitLexerTests

---
+++ src/SLANG/tests/Core/DigitLexerTests.cs
@@ -0,0 +1,30 @@
+﻿namespace SLANG.Core
+{
+    using Xunit;
+
+    public class DigitLexerTests
+    {
+        private readonly DigitLexer lexer = new DigitLexer(new FakeServiceLocator());
+
+        [Theory]
+        [InlineData("\x30")]
+        [InlineData("\x31")]
+        [InlineData("\x32")]
+        [InlineData("\x33")]
+        [InlineData("\x34")]
+        [InlineData("\x35")]
+        [InlineData("\x36")]
+        [InlineData("\x37")]
+        [InlineData("\x38")]
+        [InlineData("\x39")]
+        public void CanReadDigit(string digit)
+        {
+            using (ITextScanner textScanner = new TextScanner(new PushbackInputStream(digit.ToMemoryStream())))
+            {
+                textScanner.Read();
+                var alpha = this.lexer.Read(textScanner);
+                Assert.Equal(digit, alpha.Data);
+            }
+        }
+    }
+}
