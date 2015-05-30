6d70b9ef96d4b66408286295067cd819cdcdbae7
Steven Liekens <steven.liekens@gmail.com>
2015-05-30 22:58:48 +0200

    Adding display name values for OctetLexerTests

--- src/SLANG/tests/Core/OctetLexerTests.cs
+++ src/SLANG/tests/Core/OctetLexerTests.cs
@@ -5,24 +5,24 @@
     public class OctetLexerTests
     {
         [Theory]
-        [InlineData("\x00")]
-        [InlineData("\x10")]
-        [InlineData("\x20")]
-        [InlineData("\x30")]
-        [InlineData("\x40")]
-        [InlineData("\x50")]
-        [InlineData("\x60")]
-        [InlineData("\x70")]
-        [InlineData("\x80")]
-        [InlineData("\x90")]
-        [InlineData("\x10")]
-        [InlineData("\xA0")]
-        [InlineData("\xB0")]
-        [InlineData("\xC0")]
-        [InlineData("\xD0")]
-        [InlineData("\xE0")]
-        [InlineData("\xFF")]
-        public void ReadSuccess(string input)
+        [InlineData("\x0000", "0x00")]
+        [InlineData("\x0010", "0x10")]
+        [InlineData("\x0020", "0x20")]
+        [InlineData("\x0030", "0x30")]
+        [InlineData("\x0040", "0x40")]
+        [InlineData("\x0050", "0x50")]
+        [InlineData("\x0060", "0x60")]
+        [InlineData("\x0070", "0x70")]
+        [InlineData("\x0080", "0x80")]
+        [InlineData("\x0090", "0x90")]
+        [InlineData("\x0010", "0x10")]
+        [InlineData("\x00A0", "0xA0")]
+        [InlineData("\x00B0", "0xB0")]
+        [InlineData("\x00C0", "0xC0")]
+        [InlineData("\x00D0", "0xD0")]
+        [InlineData("\x00E0", "0xE0")]
+        [InlineData("\x00FF", "0xFF")]
+        public void ReadSuccess(string input, string displayName)
         {
             var octetLexer = new OctetLexer(new OctetValueRangeLexer());
             using (var scanner = new TextScanner(new PushbackInputStream(input.ToMemoryStream())))
