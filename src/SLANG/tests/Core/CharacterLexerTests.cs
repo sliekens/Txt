41decb504cc5b75d008bd2cd5812e130580fab67
Steven Liekens <steven.liekens@gmail.com>
2015-05-30 20:31:29 +0200

    Adding

---
+++ src/SLANG/tests/Core/CharacterLexerTests.cs
@@ -0,0 +1,28 @@
+﻿namespace SLANG.Core
+{
+    using Xunit;
+
+    public class CharacterLexerTests
+    {
+        [Theory]
+        [InlineData("\x01")]
+        [InlineData("\x10")]
+        [InlineData("\x20")]
+        [InlineData("\x30")]
+        [InlineData("\x40")]
+        [InlineData("\x50")]
+        [InlineData("\x60")]
+        [InlineData("\x70")]
+        [InlineData("\x7F")]
+        public void ReadSuccess(string c)
+        {
+            var lexer = new CharacterLexer(new CharacterValueRangeLexer());
+            using (var textScanner = new TextScanner(new PushbackInputStream(c.ToMemoryStream())))
+            {
+                var character = lexer.Read(textScanner);
+                Assert.NotNull(character);
+                Assert.Equal(c, character.Data);
+            }
+        }
+    }
+}
