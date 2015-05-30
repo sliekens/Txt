fd5778823400a9bbc681bde384fc28dd2a06aaa5
Steven Liekens <steven.liekens@gmail.com>
2015-05-30 21:37:06 +0200

    Fixing an invalid ctor signature in BreakingWhiteSpaceLexer

--- src/SLANG/src/Core/BreakingWhiteSpaceLexer.cs
+++ src/SLANG/src/Core/BreakingWhiteSpaceLexer.cs
@@ -2,7 +2,7 @@
 {
     public class BreakingWhiteSpaceLexer : AlternativeLexer
     {
-        public BreakingWhiteSpaceLexer(ILexer<EndOfLine> whiteSpaceLexer, ILexer<Sequence> endOfLineWhiteSpaceSequenceLexer)
+        public BreakingWhiteSpaceLexer(ILexer<WhiteSpace> whiteSpaceLexer, ILexer<Sequence> endOfLineWhiteSpaceSequenceLexer)
             : base(whiteSpaceLexer, endOfLineWhiteSpaceSequenceLexer)
         {
         }
