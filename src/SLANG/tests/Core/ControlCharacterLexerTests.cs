98c13d8d2133d10da685a9397fed3743df289631
Steven Liekens <steven.liekens@gmail.com>
2015-05-30 20:46:50 +0200

    Adding ControlCharacterLexerTests

---
+++ src/SLANG/tests/Core/ControlCharacterLexerTests.cs
@@ -0,0 +1,23 @@
+﻿namespace SLANG.Core
+{
+    using Xunit;
+
+    public class ControlCharacterLexerTests
+    {
+        [Theory]
+        [InlineData("\x00")]
+        [InlineData("\x10")]
+        [InlineData("\x1F")]
+        [InlineData("\x7F")]
+        public void ReadSuccess(string input)
+        {
+            var controlCharacterLexer = new ControlCharacterLexer(new ControlCharacterAlternativeLexer(new ControlsValueRangeLexer(), new DeleteTerminalLexer()));
+            using (var scanner = new TextScanner(new PushbackInputStream(input.ToMemoryStream())))
+            {
+                var controlCharacter = controlCharacterLexer.Read(scanner);
+                Assert.NotNull(controlCharacter);
+                Assert.Equal(input, controlCharacter.Data);
+            }
+        }
+    }
+}
