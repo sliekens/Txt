9916fd7639aa838aa20a2a690c79ad6b1dd6fa7d
Steven Liekens <steven.liekens@gmail.com>
2015-05-30 20:44:33 +0200

    Adding EndOfLineLexerTests

---
+++ src/SLANG/tests/Core/EndOfLineLexerTests.cs
@@ -0,0 +1,20 @@
+﻿namespace SLANG.Core
+{
+    using Xunit;
+
+    public class EndOfLineLexerTests
+    {
+        [Theory]
+        [InlineData("\r\n")]
+        public void ReadSuccess(string input)
+        {
+            var endOfLineLexer = new EndOfLineLexer(new EndOfLineSequenceLexer(new CarriageReturnLexer(new CarriageReturnTerminalLexer()), new LineFeedLexer(new LineFeedTerminalLexer())));
+            using (var scanner = new TextScanner(new PushbackInputStream(input.ToMemoryStream())))
+            {
+                var endOfLine = endOfLineLexer.Read(scanner);
+                Assert.NotNull(endOfLine);
+                Assert.Equal(input, endOfLine.Data);
+            }
+        }
+    }
+}
