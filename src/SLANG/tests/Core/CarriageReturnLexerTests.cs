498aa97c82100147d3f8a1e1fb23b3986c21dccd
Steven Liekens <steven.liekens@gmail.com>
2015-05-30 20:36:09 +0200

    Adding CarriageReturnLexerTests

---
+++ src/SLANG/tests/Core/CarriageReturnLexerTests.cs
@@ -0,0 +1,20 @@
+﻿namespace SLANG.Core
+{
+    using Xunit;
+
+    public class CarriageReturnLexerTests
+    {
+        [Theory]
+        [InlineData("\x0D")]
+        public void ReadSuccess(string input)
+        {
+            var carriageReturnLexer = new CarriageReturnLexer(new CarriageReturnTerminalLexer());
+            using (var scanner = new TextScanner(new PushbackInputStream(input.ToMemoryStream())))
+            {
+                var carriageReturn = carriageReturnLexer.Read(scanner);
+                Assert.NotNull(carriageReturn);
+                Assert.Equal(input, carriageReturn.Data);
+            }
+        }
+    }
+}
