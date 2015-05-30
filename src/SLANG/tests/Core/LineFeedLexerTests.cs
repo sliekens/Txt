529673a128df36b7070eaafe3682328a2a21b7d4
Steven Liekens <steven.liekens@gmail.com>
2015-05-30 20:38:56 +0200

    Adding LineFeedLexerTests

---
+++ src/SLANG/tests/Core/LineFeedLexerTests.cs
@@ -0,0 +1,20 @@
+﻿namespace SLANG.Core
+{
+    using Xunit;
+
+    public class LineFeedLexerTests
+    {
+        [Theory]
+        [InlineData("\x0A")]
+        public void ReadSuccess(string input)
+        {
+            var lineFeedLexer = new LineFeedLexer(new LineFeedTerminalLexer());
+            using (var scanner = new TextScanner(new PushbackInputStream(input.ToMemoryStream())))
+            {
+                var lineFeed = lineFeedLexer.Read(scanner);
+                Assert.NotNull(lineFeed);
+                Assert.Equal(input, lineFeed.Data);
+            }
+        }
+    }
+}
