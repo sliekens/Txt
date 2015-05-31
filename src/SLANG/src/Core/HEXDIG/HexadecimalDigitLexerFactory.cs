22f1193a3a17dcb124bbe63dba0495b471fa81e6
Steven Liekens <steven.liekens@gmail.com>
2015-05-31 22:47:03 +0200

    Adding HexadecimalDigitLexerFactory

---
+++ src/SLANG/src/Core/HEXDIG/HexadecimalDigitLexerFactory.cs
@@ -0,0 +1,53 @@
+﻿namespace SLANG.Core.HEXDIG
+{
+    using System;
+
+    public class HexadecimalDigitLexerFactory : ILexerFactory<HexadecimalDigit>
+    {
+        private readonly IAlternativeLexerFactory alternativeLexerFactory;
+
+        private readonly ILexer<Digit> digitLexer;
+
+        private readonly ITerminalsLexerFactory terminalsLexerFactory;
+
+        public HexadecimalDigitLexerFactory(
+            ILexer<Digit> digitLexer,
+            ITerminalsLexerFactory terminalsLexerFactory,
+            IAlternativeLexerFactory alternativeLexerFactory)
+        {
+            if (digitLexer == null)
+            {
+                throw new ArgumentNullException("digitLexer", "Precondition: digitLexer != null");
+            }
+
+            if (terminalsLexerFactory == null)
+            {
+                throw new ArgumentNullException("terminalsLexerFactory", "Precondition: terminalsLexerFactory != null");
+            }
+
+            if (alternativeLexerFactory == null)
+            {
+                throw new ArgumentNullException(
+                    "alternativeLexerFactory",
+                    "Precondition: alternativeLexerFactory != null");
+            }
+
+            this.digitLexer = digitLexer;
+            this.terminalsLexerFactory = terminalsLexerFactory;
+            this.alternativeLexerFactory = alternativeLexerFactory;
+        }
+
+        public ILexer<HexadecimalDigit> Create()
+        {
+            var hexadecimalDigitAlternativeLexer = this.alternativeLexerFactory.Create(
+                this.digitLexer,
+                this.terminalsLexerFactory.Create("A"),
+                this.terminalsLexerFactory.Create("B"),
+                this.terminalsLexerFactory.Create("C"),
+                this.terminalsLexerFactory.Create("D"),
+                this.terminalsLexerFactory.Create("E"),
+                this.terminalsLexerFactory.Create("F"));
+            return new HexadecimalDigitLexer(hexadecimalDigitAlternativeLexer);
+        }
+    }
+}
