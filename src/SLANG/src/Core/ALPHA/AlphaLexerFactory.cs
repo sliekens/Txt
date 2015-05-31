6c7f55c4ebe3fa47458674d01e76007f3fd54fa5
Steven Liekens <steven.liekens@gmail.com>
2015-05-31 21:55:23 +0200

    Adding AlphaLexerFactory

---
+++ src/SLANG/src/Core/ALPHA/AlphaLexerFactory.cs
@@ -0,0 +1,35 @@
+﻿namespace SLANG.Core.ALPHA
+{
+    using System;
+
+    public class AlphaLexerFactory : ILexerFactory<Alpha>
+    {
+        private readonly IValueRangeLexerFactory valueRangeLexerFactory;
+
+        private readonly IAlternativeLexerFactory alternativeLexerFactory;
+
+        public AlphaLexerFactory(IValueRangeLexerFactory valueRangeLexerFactory, IAlternativeLexerFactory alternativeLexerFactory)
+        {
+            if (valueRangeLexerFactory == null)
+            {
+                throw new ArgumentNullException("valueRangeLexerFactory", "Precondition: valueRangeLexerFactory != null");
+            }
+
+            if (alternativeLexerFactory == null)
+            {
+                throw new ArgumentNullException("alternativeLexerFactory", "Precondition: alternativeLexerFactory != null");
+            }
+
+            this.valueRangeLexerFactory = valueRangeLexerFactory;
+            this.alternativeLexerFactory = alternativeLexerFactory;
+        }
+
+        public ILexer<Alpha> Create()
+        {
+            var upperCaseValueRangeLexer = this.valueRangeLexerFactory.Create('\x41', '\x5A');
+            var lowerCaseValueRangeLexer = this.valueRangeLexerFactory.Create('\x61', '\x7A');
+            var upperOrLowerCaseAlphaLexer = this.alternativeLexerFactory.Create(upperCaseValueRangeLexer, lowerCaseValueRangeLexer);
+            return new AlphaLexer(upperOrLowerCaseAlphaLexer);
+        }
+    }
+}
