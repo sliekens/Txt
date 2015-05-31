1fd8bf20347d5c3cf861e857f5a9f9d18b6dcb70
Steven Liekens <steven.liekens@gmail.com>
2015-05-31 22:42:47 +0200

    Adding 'OctetLexerFactory'

---
+++ src/SLANG/src/Core/OCTET/OctetLexerFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace SLANG.Core.OCTET
+{
+    using System;
+
+    public class OctetLexerFactory : ILexerFactory<Octet>
+    {
+        private readonly IValueRangeLexerFactory valueRangeLexerFactory;
+
+        public OctetLexerFactory(IValueRangeLexerFactory valueRangeLexerFactory)
+        {
+            if (valueRangeLexerFactory == null)
+            {
+                throw new ArgumentNullException("valueRangeLexerFactory", "Precondition: valueRangeLexerFactory != null");
+            }
+
+            this.valueRangeLexerFactory = valueRangeLexerFactory;
+        }
+
+        public ILexer<Octet> Create()
+        {
+            var octetValueRangeLexer = this.valueRangeLexerFactory.Create('\x00', '\xFF');
+            return new OctetLexer(octetValueRangeLexer);
+        }
+    }
+}
