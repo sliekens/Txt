54b4972dff2306955944b696f8d0136145dee02e
Steven Liekens <steven.liekens@gmail.com>
2015-05-31 20:01:22 +0200

    Adding comments to LinearWhiteSpaceLexerTests

--- src/SLANG/tests/Core/LinearWhiteSpaceLexerTests.cs
+++ src/SLANG/tests/Core/LinearWhiteSpaceLexerTests.cs
@@ -14,21 +14,40 @@
         [InlineData("\t\r\n \t \r\n  \t ")]
         public void ReadSuccess(string input)
         {
+            // SP
             var spaceTerminalLexer = new TerminalsLexer('\x20');
             var spaceLexer = new SpaceLexer(spaceTerminalLexer);
+
+            // HTAB
             var horizontalTabTerminalLexer = new TerminalsLexer('\x09');
             var horizontalTabLexer = new HorizontalTabLexer(horizontalTabTerminalLexer);
+
+            // WSP
             var whiteSpaceAlternativeLexer = new AlternativeLexer(spaceLexer, horizontalTabLexer);
             var whiteSpaceLexer = new WhiteSpaceLexer(whiteSpaceAlternativeLexer);
+
+            // CR
             var carriageReturnTerminalLexer = new TerminalsLexer('\x0D');
             var carriageReturnLexer = new CarriageReturnLexer(carriageReturnTerminalLexer);
+
+            // LF
             var lineFeedTerminalLexer = new TerminalsLexer('\x0A');
             var lineFeedLexer = new LineFeedLexer(lineFeedTerminalLexer);
+
+            // CRLF
             var endOfLineSequenceLexer = new SequenceLexer(carriageReturnLexer, lineFeedLexer);
             var endOfLineLexer = new EndOfLineLexer(endOfLineSequenceLexer);
+
+            // CRLF WSP
             var endOfLineWhiteSpaceSequenceLexer = new SequenceLexer(endOfLineLexer, whiteSpaceLexer);
+
+            // WSP / CRLF WSP
             var breakingWhiteSpaceLexer = new AlternativeLexer(whiteSpaceLexer, endOfLineWhiteSpaceSequenceLexer);
+
+            // *( WSP / CRLF WSP )
             var linearWhiteSpaceRepetitionLexer = new RepetitionLexer(breakingWhiteSpaceLexer, 0, int.MaxValue);
+
+            // LWSP
             var lexer = new LinearWhiteSpaceLexer(linearWhiteSpaceRepetitionLexer);
             using (var scanner = new TextScanner(new PushbackInputStream(input.ToMemoryStream())))
             {
