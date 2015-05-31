e2ad3f3b3a09976110e9db0d0a9f66c9076c9c0d
Steven Liekens <steven.liekens@gmail.com>
2015-05-31 22:48:18 +0200

    Adding DoubleQuoteLexerFactory

---
+++ src/SLANG/src/Core/DQUOTE/DoubleQuoteLexerFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace SLANG.Core.DQUOTE
+{
+    using System;
+
+    public class DoubleQuoteLexerFactory : ILexerFactory<DoubleQuote>
+    {
+        private readonly ITerminalsLexerFactory terminalsLexerFactory;
+
+        public DoubleQuoteLexerFactory(ITerminalsLexerFactory terminalsLexerFactory)
+        {
+            if (terminalsLexerFactory == null)
+            {
+                throw new ArgumentNullException("terminalsLexerFactory", "Precondition: terminalsLexerFactory != null");
+            }
+
+            this.terminalsLexerFactory = terminalsLexerFactory;
+        }
+
+        public ILexer<DoubleQuote> Create()
+        {
+            var doubleQuoteTerminalLexer = this.terminalsLexerFactory.Create('\x22');
+            return new DoubleQuoteLexer(doubleQuoteTerminalLexer);
+        }
+    }
+}
