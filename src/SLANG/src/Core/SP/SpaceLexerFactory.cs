852e1fb784f91b2031f4a8de77bf3f3eb407ba4c
Steven Liekens <steven.liekens@gmail.com>
2015-05-31 22:40:24 +0200

    Adding SpaceLexerFactory

---
+++ src/SLANG/src/Core/SP/SpaceLexerFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace SLANG.Core.SP
+{
+    using System;
+
+    public class SpaceLexerFactory : ILexerFactory<Space>
+    {
+        private readonly ITerminalsLexerFactory terminalsLexerFactory;
+
+        public SpaceLexerFactory(ITerminalsLexerFactory terminalsLexerFactory)
+        {
+            if (terminalsLexerFactory == null)
+            {
+                throw new ArgumentNullException("terminalsLexerFactory", "Precondition: terminalsLexerFactory != null");
+            }
+
+            this.terminalsLexerFactory = terminalsLexerFactory;
+        }
+
+        public ILexer<Space> Create()
+        {
+            var spaceTerminalLexer = this.terminalsLexerFactory.Create('\x20');
+            return new SpaceLexer(spaceTerminalLexer);
+        }
+    }
+}
