2ecd4999bf5630dad6a371e3450b07c5e004b698
Steven Liekens <steven.liekens@gmail.com>
2015-05-31 22:44:03 +0200

    Adding HorizontalTabLexerFactory

---
+++ src/SLANG/src/Core/HTAB/HorizontalTabLexerFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace SLANG.Core.HTAB
+{
+    using System;
+
+    public class HorizontalTabLexerFactory : ILexerFactory<HorizontalTab>
+    {
+        private readonly ITerminalsLexerFactory terminalsLexerFactory;
+
+        public HorizontalTabLexerFactory(ITerminalsLexerFactory terminalsLexerFactory)
+        {
+            if (terminalsLexerFactory == null)
+            {
+                throw new ArgumentNullException("terminalsLexerFactory", "Precondition: terminalsLexerFactory != null");
+            }
+
+            this.terminalsLexerFactory = terminalsLexerFactory;
+        }
+
+        public ILexer<HorizontalTab> Create()
+        {
+            var horizontalTabTerminalLexer = this.terminalsLexerFactory.Create('\x09');
+            return new HorizontalTabLexer(horizontalTabTerminalLexer);
+        }
+    }
+}
