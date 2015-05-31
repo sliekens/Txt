33053a447520c94383017b66e5a7426221368187
Steven Liekens <steven.liekens@gmail.com>
2015-05-31 22:39:14 +0200

    Adding VisibleCharacterLexerFactory

---
+++ src/SLANG/src/Core/VCHAR/VisibleCharacterLexerFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace SLANG.Core.VCHAR
+{
+    using System;
+
+    public class VisibleCharacterLexerFactory : ILexerFactory<VisibleCharacter>
+    {
+        private readonly IValueRangeLexerFactory valueRangeLexer;
+
+        public VisibleCharacterLexerFactory(IValueRangeLexerFactory valueRangeLexer)
+        {
+            if (valueRangeLexer == null)
+            {
+                throw new ArgumentNullException("valueRangeLexer", "Precondition: valueRangeLexer != null");
+            }
+
+            this.valueRangeLexer = valueRangeLexer;
+        }
+
+        public ILexer<VisibleCharacter> Create()
+        {
+            return new VisibleCharacterLexer(this.valueRangeLexer.Create('\x21', '\x7E'));
+        }
+    }
+}
