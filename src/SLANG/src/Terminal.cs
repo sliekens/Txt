045b0f1bd5cdcfed56ba36e4613c17809b3c6709
Steven Liekens <steven.liekens@gmail.com>
2015-06-02 22:45:28 +0200

    Adding classes 'Terminal' and 'TerminalString'

---
+++ src/SLANG/src/Terminal.cs
@@ -0,0 +1,15 @@
+﻿namespace SLANG
+{
+    public class Terminal : Element
+    {
+        public Terminal(Terminal element)
+            : base(element)
+        {
+        }
+
+        public Terminal(char data, ITextContext context)
+            : base(data, context)
+        {
+        }
+    }
+}
---
+++ src/SLANG/src/TerminalString.cs
@@ -0,0 +1,28 @@
+﻿namespace SLANG
+{
+    using System.Collections.Generic;
+
+    public class TerminalString : Element
+    {
+        private readonly IList<Terminal> terminals;
+
+        public TerminalString(TerminalString element)
+            : base(element)
+        {
+        }
+
+        public TerminalString(IList<Terminal> terminals, ITextContext context)
+            : base(string.Concat(terminals), context)
+        {
+            this.terminals = terminals;
+        }
+
+        public IList<Terminal> Terminals
+        {
+            get
+            {
+                return this.terminals;
+            }
+        }
+    }
+}
