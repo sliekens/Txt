f22f158ca73f3eb91d8fcd0e1db71245765c5e37
Steven Liekens <steven.liekens@gmail.com>
2015-06-02 20:19:11 +0200

    Adding Bit.ToBool()

--- src/SLANG/src/Core/BIT/Bit.cs
+++ src/SLANG/src/Core/BIT/Bit.cs
@@ -6,13 +6,27 @@
 //   
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
+
 namespace SLANG.Core.BIT
 {
+    using System.Diagnostics;
+
     public class Bit : Alternative
     {
         public Bit(Element element)
             : base(element)
         {
+        }
+
+        public bool ToBool()
+        {
+            Debug.Assert(this.Data == "0" || this.Data == "1", "this.Data == '0' || this.Data == '1'");
+            if (this.Data == "1")
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
