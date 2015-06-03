51445a2bc387ea1e3f4c1dcc4bdbbbc5b6d5c5fc
Steven Liekens <steven.liekens@external.atlascopco.com>
2015-06-03 14:46:27 +0200

    Adding WhiteSpaceLexerTests

---
+++ src/SLANG/tests/Core/WSP/WhiteSpaceLexerTests.cs
@@ -0,0 +1,33 @@
+﻿namespace SLANG.Core.WSP
+{
+    using SLANG.Core.HTAB;
+    using SLANG.Core.SP;
+
+    using Xunit;
+
+    public class WhiteSpaceLexerTests
+    {
+        [Theory]
+        [InlineData("\x20")]
+        [InlineData(" ")]
+        [InlineData("\x09")]
+        [InlineData("\t")]
+        [InlineData(@"	")]
+        public void ReadSuccess(string input)
+        {
+            var terminalLexerFactory = new TerminalLexerFactory();
+            var spaceLexerFactory = new SpaceLexerFactory(terminalLexerFactory);
+            var horizontalTabLexerFactory = new HorizontalTabLexerFactory(terminalLexerFactory);
+            var alternativeLexerFactory = new AlternativeLexerFactory();
+            var whiteSpaceLexerFactory = new WhiteSpaceLexerFactory(spaceLexerFactory, horizontalTabLexerFactory, alternativeLexerFactory);
+            var whiteSpaceLexer = whiteSpaceLexerFactory.Create();
+            using (var scanner = new TextScanner(new PushbackInputStream(input.ToMemoryStream())))
+            {
+                scanner.Read();
+                var whiteSpace = whiteSpaceLexer.Read(scanner);
+                Assert.NotNull(whiteSpace);
+                Assert.Equal(input, whiteSpace.Data);
+            }
+        }
+    }
+}
