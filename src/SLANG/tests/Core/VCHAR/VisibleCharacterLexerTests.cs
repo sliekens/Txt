efb93ac5fe2b0e907b800c1353ef1057f1a76962
Steven Liekens <steven.liekens@external.atlascopco.com>
2015-06-03 14:43:43 +0200

    Adding VisibleCharacterLexerTests

---
+++ src/SLANG/tests/Core/VCHAR/VisibleCharacterLexerTests.cs
@@ -0,0 +1,23 @@
+﻿namespace SLANG.Core.VCHAR
+{
+    using Xunit;
+
+    public class VisibleCharacterLexerTests
+    {
+        [Theory]
+        [InlineData("\x21")]
+        [InlineData("\x7E")]
+        public void ReadSuccess(string input)
+        {
+            var factory = new VisibleCharacterLexerFactory(new ValueRangeLexerFactory());
+            var lineFeedLexer = factory.Create();
+            using (var scanner = new TextScanner(new PushbackInputStream(input.ToMemoryStream())))
+            {
+                scanner.Read();
+                var visibleCharacter = lineFeedLexer.Read(scanner);
+                Assert.NotNull(visibleCharacter);
+                Assert.Equal(input, visibleCharacter.Data);
+            }
+        }
+    }
+}
