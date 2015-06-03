db2399be973b3852c64599383adefcd03fc79b00
Steven Liekens <steven.liekens@external.atlascopco.com>
2015-06-03 13:08:52 +0200

    Adding explicit cast support for (char)Terminal

--- src/SLANG/src/Terminal.cs
+++ src/SLANG/src/Terminal.cs
@@ -15,6 +15,11 @@
             this.terminal = data;
         }
 
+        public static explicit operator char(Terminal instance)
+        {
+            return instance.terminal;
+        }
+
         public char ToChar()
         {
             return this.terminal;
