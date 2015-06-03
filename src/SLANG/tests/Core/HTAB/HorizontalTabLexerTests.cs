55a215c9cc49a1d3c64cdecae34234c36effb0ff
Steven Liekens <steven.liekens@external.atlascopco.com>
2015-06-03 14:37:25 +0200

    Adding HorizontalTabLexerTests

---
+++ src/SLANG/tests/Core/HTAB/HorizontalTabLexerTests.cs
@@ -0,0 +1,24 @@
+﻿namespace SLANG.Core.HTAB
+{
+    using SLANG.Core.DQUOTE;
+
+    using Xunit;
+
+    public class HorizontalTabLexerTests
+    {
+        [Theory]
+        [InlineData("\x09")]
+        [InlineData("\t")]
+        public void ReadSuccess(string input)
+        {
+            var factory = new HorizontalTabLexerFactory(new TerminalLexerFactory());
+            var lexer = factory.Create();
+            using (var scanner = new TextScanner(new PushbackInputStream(input.ToMemoryStream())))
+            {
+                scanner.Read();
+                var horizontalTab = lexer.Read(scanner);
+                Assert.Equal(input, horizontalTab.Data);
+            }
+        }
+    }
+}
