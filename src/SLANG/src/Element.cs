d0be8a79cb64cb67b7d8e6301538e7d83106c189
Steven Liekens <steven.liekens@gmail.com>
2015-06-06 21:08:24 +0200

    Adding xml doc comments

--- src/SLANG/src/Element.cs
+++ src/SLANG/src/Element.cs
@@ -3,14 +3,18 @@
     using System;
     using System.Diagnostics;
 
+    /// <summary>Provides the base class for all elements.</summary>
     public abstract class Element : ITextContext
     {
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private readonly string values;
-
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private readonly int offset;
 
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly string values;
+
+        /// <summary>Initializes a new instance of the <see cref="Element"/> class with a given element to copy.</summary>
+        /// <param name="element">The element to copy.</param>
+        /// <exception cref="ArgumentNullException">The value of <paramref name="element"/> is a null reference.</exception>
         protected Element(Element element)
         {
             if (element == null)
@@ -22,6 +26,10 @@
             this.offset = element.offset;
         }
 
+        /// <summary>Initializes a new instance of the <see cref="Element"/> class with a given terminal and context.</summary>
+        /// <param name="value">The terminal value.</param>
+        /// <param name="context">An object that describes the current element's context.</param>
+        /// <exception cref="ArgumentNullException">The value of <paramref name="context"/> is a null reference.</exception>
         protected Element(char value, ITextContext context)
         {
             if (context == null)
@@ -33,6 +41,10 @@
             this.offset = context.Offset;
         }
 
+        /// <summary>Initializes a new instance of the <see cref="Element"/> class with the given terminal values and context.</summary>
+        /// <param name="values">The terminal values.</param>
+        /// <param name="context">An object that describes the current element's context.</param>
+        /// <exception cref="ArgumentNullException">The value of <paramref name="values"/> or <paramref name="context"/> is a null reference.</exception>
         protected Element(string values, ITextContext context)
         {
             if (values == null)
@@ -49,15 +61,6 @@
             this.offset = context.Offset;
         }
 
-        public string Values
-        {
-            get
-            {
-                Debug.Assert(this.values != null);
-                return this.values;
-            }
-        }
-
         /// <summary>Gets the current position, relative to the beginning of the data source.</summary>
         public int Offset
         {
@@ -67,12 +70,23 @@
             }
         }
 
+        /// <summary>Gets one or more terminal values that represent the current element.</summary>
+        public string Values
+        {
+            get
+            {
+                Debug.Assert(this.values != null);
+                return this.values;
+            }
+        }
+
         /// <summary>
         ///     Gets a well-formed string that represents the current element. This is useful for elements that are
         ///     technically valid, but contain formatting errors or other inpurities. For example: mixed upper and lower case
         ///     characters where only lower case is well-formed. Unless overridden, the default return value is the value of
         ///     <see cref="Values" />.
         /// </summary>
+        /// <returns>A well-formed string that represents the current element.</returns>
         public virtual string GetWellFormedData()
         {
             return this.Values;
