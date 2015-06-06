6df712e081110c296afe5b8676293b3bd28e341f
Steven Liekens <steven.liekens@gmail.com>
2015-06-06 20:48:46 +0200

    Adding xml doc comments

--- src/SLANG/src/RuleNameAttribute.cs
+++ src/SLANG/src/RuleNameAttribute.cs
@@ -2,16 +2,24 @@
 {
     using System;
 
+    /// <summary>Used to associate an implementation of <see cref="ILexer" /> with a grammar rule.</summary>
     [AttributeUsage(AttributeTargets.Class)]
     public sealed class RuleNameAttribute : Attribute
     {
         private readonly string ruleName;
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RuleNameAttribute" /> with a given rule name.
+        /// </summary>
+        /// <param name="ruleName">The name of the grammar rule.</param>
         public RuleNameAttribute(string ruleName)
         {
             this.ruleName = ruleName;
         }
 
+        /// <summary>
+        ///     Gets the name of the grammar rule.
+        /// </summary>
         public string RuleName
         {
             get
