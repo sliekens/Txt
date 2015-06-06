92c927822f12459c2b71dd09eed6622650a718d9
Steven Liekens <steven.liekens@gmail.com>
2015-06-06 19:41:15 +0200

    Adding xml doc comments

--- src/SLANG/src/Terminal.cs
+++ src/SLANG/src/Terminal.cs
@@ -1,14 +1,26 @@
 ﻿namespace SLANG
 {
+    /// <summary>
+    ///     Represents a terminal specification, sometimes called a character.
+    /// </summary>
     public class Terminal : Element
     {
         private readonly char terminal;
 
+        /// <summary>
+        ///     Creates a new instance of the <see cref="Terminal" /> class with a specified terminal to copy.
+        /// </summary>
+        /// <param name="element">The terminal element to copy.</param>
         public Terminal(Terminal element)
             : base(element)
         {
         }
 
+        /// <summary>
+        ///     Creates a new instance of the <see cref="Terminal" /> class with a specified character and context.
+        /// </summary>
+        /// <param name="data">The terminal value.</param>
+        /// <param name="context">An object that describes the current element's context.</param>
         public Terminal(char data, ITextContext context)
             : base(data, context)
         {
@@ -20,6 +32,8 @@
             return instance.terminal;
         }
 
+        /// <summary>Converts the value of this instance to a value of type <see cref="char" />.</summary>
+        /// <returns>The converted value.</returns>
         public char ToChar()
         {
             return this.terminal;
