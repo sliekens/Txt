8e395e962735aff8b94fa298f2443e8d18a84c45
Steven Liekens <steven.liekens@gmail.com>
2015-06-06 19:55:12 +0200

    Adding xml doc comments

--- src/SLANG/src/TerminalString.cs
+++ src/SLANG/src/TerminalString.cs
@@ -4,15 +4,27 @@
     using System.Collections.Generic;
     using System.Diagnostics;
 
+    /// <summary>
+    ///     Represents a string of terminal values.
+    /// </summary>
     public class TerminalString : Element
     {
         private readonly IList<Terminal> terminals;
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="TerminalString" /> class with a specified terminal string to copy.
+        /// </summary>
+        /// <param name="element">The terminal string to copy.</param>
         public TerminalString(TerminalString element)
             : base(element)
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="TerminalString" /> class with the given terminal values and context.
+        /// </summary>
+        /// <param name="terminals">The terminal values, in order of appearance.</param>
+        /// <param name="context">An object that describes the current element's context.</param>
         public TerminalString(IList<Terminal> terminals, ITextContext context)
             : base(string.Concat(terminals), context)
         {
@@ -24,6 +36,9 @@
             this.terminals = terminals;
         }
 
+        /// <summary>
+        ///     Gets the terminal values in the string, in order of appearance.
+        /// </summary>
         public IList<Terminal> Terminals
         {
             get
