e2f47372008f1e0074c567a199895f1bf1c966f6
Steven Liekens <steven.liekens@gmail.com>
2015-06-06 19:34:30 +0200

    Improving value range lexing classes

--- src/SLANG/src/ValueRangeLexerFactory.cs
+++ src/SLANG/src/ValueRangeLexerFactory.cs
@@ -1,9 +1,18 @@
 namespace SLANG
 {
+    using System;
+
+    /// <summary>Creates instances of the <see cref="ValueRangeLexer" /> class.</summary>
     public class ValueRangeLexerFactory : IValueRangeLexerFactory
     {
+        /// <inheritdoc />
         public ILexer<Terminal> Create(char lowerBound, char upperBound)
         {
+            if (upperBound < lowerBound)
+            {
+                throw new ArgumentOutOfRangeException("upperBound", "Precondition: upperBound >= lowerBound");
+            }
+
             return new ValueRangeLexer(lowerBound, upperBound);
         }
     }
