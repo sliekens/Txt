00d07cf051dc0643c935bbafc0b91c80edce4649
Steven Liekens <steven.liekens@gmail.com>
2015-06-07 09:35:36 +0200

    Moving ILexer<T> to its own file

--- src/SLANG/src/ILexer{TElement}.cs
+++ src/SLANG/src/ILexer{TElement}.cs
@@ -1,6 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ILexer.cs" company="Steven Liekens">
+//   The MIT License (MIT)
+// </copyright>
+// <summary>
+//   Provides the interface for types that process the lexical syntax of a language.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace SLANG
 {
-    public interface ILexer
+    /// <summary>Provides the interface for types that process the lexical syntax of a language.</summary>
+    /// <typeparam name="TElement">The type of the element that represents the lexer rule.</typeparam>
+    public interface ILexer<TElement> : ILexer
+        where TElement : Element
     {
         /// <summary>Reads the next element.</summary>
         /// <param name="scanner">
@@ -13,7 +25,7 @@ namespace SLANG
         ///     end of the text source.
         /// </exception>
         /// <returns>The next available element.</returns>
-        Element ReadElement(ITextScanner scanner);
+        TElement Read(ITextScanner scanner);
 
         /// <summary>Attempts to read the next element. A return value indicates whether the element was available.</summary>
         /// <param name="scanner">
@@ -27,6 +39,6 @@ namespace SLANG
         /// <exception cref="T:System.InvalidOperationException">The given scanner object is not initialized.</exception>
         /// <exception cref="T:System.ObjectDisposedException">The given text scanner is closed.</exception>
         /// <returns><c>true</c> to indicate success; otherwise, <c>false</c>.</returns>
-        bool TryReadElement(ITextScanner scanner, out Element element);
+        bool TryRead(ITextScanner scanner, out TElement element);
     }
 }
