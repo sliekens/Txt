b88e9f6b272f847442287c121f737bc7b63b6286
Steven Liekens <steven.liekens@gmail.com>
2015-06-07 14:35:55 +0200

    Fixing nuget configuration

--- src/TextFx.ABNF/Properties/AssemblyInfo.cs
+++ src/TextFx.ABNF/Properties/AssemblyInfo.cs
@@ -1,9 +1,8 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TextFx.ABNF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Implements RFC 5234")]
 [assembly: AssemblyCulture("")]
