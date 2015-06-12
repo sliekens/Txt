bd3ef1870aaf3f209571a0282215332eaea98800
Steven Liekens <steven.liekens@gmail.com>
2015-06-12 17:12:55 +0200

    Improving documentation

--- src/TextFx/PushbackInputStream.cs
+++ src/TextFx/PushbackInputStream.cs
@@ -67,16 +67,20 @@
         }
 
         /// <summary>Gets a value indicating whether the current stream supports writing.</summary>
-        /// <returns></returns>
+        /// <returns><c>true</c> if the stream supports pushing back bytes; <c>false</c> if the stream supports seeking.</returns>
         public override bool CanWrite
         {
             get
             {
-                // MEMO: 'Write' means 'Unread'; if CanSeek, then the caller should Seek() instead of Write()
+                // MEMO: 'Write' means 'Unread' -> if CanSeek, then the caller should Seek() instead of Write()
                 return !this.stream.CanSeek;
             }
         }
 
+        /// <summary>Gets the length in bytes of the underlying stream.</summary>
+        /// <returns>A long value representing the length of the stream in bytes.</returns>
+        /// <exception cref="T:System.NotSupportedException">A class derived from Stream does not support seeking. </exception>
+        /// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
         public override long Length
         {
             get
@@ -85,6 +89,7 @@
             }
         }
 
+        // BUG: PushbackInputStream.Position does not take the pushback buffer into consideration
         public override long Position
         {
             get
@@ -97,12 +102,25 @@
             }
         }
 
+        /// <summary>Clears all buffers for this stream and causes any buffered data to be written to the underlying device.</summary>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
         public override void Flush()
         {
             this.stream.Flush();
             this.pushbackStack.TrimExcess();
         }
 
+        /// <summary>Reads a sequence of bytes from the current stream and advances the position within the stream by the number of bytes read.</summary>
+        /// <returns>The total number of bytes read into the buffer. This can be less than the number of bytes requested if that many bytes are not currently available, or zero (0) if the end of the stream has been reached.</returns>
+        /// <param name="buffer">An array of bytes. When this method returns, the buffer contains the specified byte array with the values between <paramref name="offset"/> and (<paramref name="offset"/> + <paramref name="count"/> - 1) replaced by the bytes read from the current source. </param>
+        /// <param name="offset">The zero-based byte offset in <paramref name="buffer"/> at which to begin storing the data read from the current stream. </param>
+        /// <param name="count">The maximum number of bytes to be read from the current stream. </param>
+        /// <exception cref="T:System.ArgumentException">The sum of <paramref name="offset"/> and <paramref name="count"/> is larger than the buffer length. </exception>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="buffer"/> is null. </exception>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"><paramref name="offset"/> or <paramref name="count"/> is negative. </exception>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+        /// <exception cref="T:System.NotSupportedException">The stream does not support reading. </exception>
+        /// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
         public override int Read(byte[] buffer, int offset, int count)
         {
             int result;
@@ -114,6 +132,13 @@
             return this.stream.Read(buffer, offset, count);
         }
 
+        /// <summary>Sets the position within the current stream.</summary>
+        /// <returns>The new position within the current stream.</returns>
+        /// <param name="offset">A byte offset relative to the <paramref name="origin"/> parameter. </param>
+        /// <param name="origin">A value of type <see cref="T:System.IO.SeekOrigin"/> indicating the reference point used to obtain the new position. </param>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+        /// <exception cref="T:System.NotSupportedException">The stream does not support seeking, such as if the stream is constructed from a pipe or console output. </exception>
+        /// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
         public override long Seek(long offset, SeekOrigin origin)
         {
             return this.stream.Seek(offset, origin);
@@ -123,7 +148,7 @@
         /// <param name="value">The desired length of the underlying stream in bytes. </param>
         /// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
         /// <exception cref="T:System.NotSupportedException">The stream does not support both writing and seeking, such as if the stream is constructed from a pipe or console output. </exception>
-        /// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+        /// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed.</exception>
         public override void SetLength(long value)
         {
             this.stream.SetLength(value);
