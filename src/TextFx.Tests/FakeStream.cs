7da8e162557751615c679b3cea02039962489e11
Steven Liekens <steven.liekens@gmail.com>
2015-06-13 14:21:14 +0200

    Adding PushbackInputStreamTests

---
+++ src/TextFx.Tests/FakeStream.cs
@@ -0,0 +1,154 @@
+﻿namespace TextFx.Tests
+{
+    using System;
+    using System.IO;
+
+    internal class FakeStream : Stream
+    {
+        public override bool CanRead
+        {
+            get
+            {
+                if (this.OnCanReadGet == null)
+                {
+                    throw new NotImplementedException();
+                }
+
+                return this.OnCanReadGet();
+            }
+        }
+
+        public override bool CanSeek
+        {
+            get
+            {
+                if (this.OnCanSeekGet == null)
+                {
+                    throw new NotImplementedException();
+                }
+
+                return this.OnCanSeekGet();
+            }
+        }
+
+        public override bool CanWrite
+        {
+            get
+            {
+                if (this.OnCanWriteGet == null)
+                {
+                    throw new NotImplementedException();
+                }
+
+                return this.OnCanWriteGet();
+            }
+        }
+
+        public override long Length
+        {
+            get
+            {
+                if (this.OnLengthGet == null)
+                {
+                    throw new NotImplementedException();
+                }
+
+                return this.OnLengthGet();
+            }
+        }
+
+        public Func<bool> OnCanReadGet { get; set; }
+
+        public Func<bool> OnCanSeekGet { get; set; }
+
+        public Func<bool> OnCanWriteGet { get; set; }
+
+        public Action OnFlush { get; set; }
+
+        public Func<long> OnLengthGet { get; set; }
+
+        public Func<long> OnPositionGet { get; set; }
+
+        public Action<long> OnPositionSet { get; set; }
+
+        public Func<byte[], int, int, int> OnRead { get; set; }
+
+        public Func<long, SeekOrigin, long> OnSeek { get; set; }
+
+        public Action<long> OnSetLength { get; set; }
+
+        public Action<byte[], int, int> OnWrite { get; set; }
+
+        public override long Position
+        {
+            get
+            {
+                if (this.OnPositionGet == null)
+                {
+                    throw new NotImplementedException();
+                }
+
+                return this.OnPositionGet();
+            }
+            set
+            {
+                if (this.OnPositionSet == null)
+                {
+                    throw new NotImplementedException();
+                }
+
+                this.OnPositionSet(value);
+            }
+        }
+
+        public override void Flush()
+        {
+            if (this.OnFlush == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            this.OnFlush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (this.OnRead == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            return this.OnRead(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            if (this.OnSeek == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            return this.OnSeek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            if (this.OnSetLength == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            this.OnSetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (this.OnWrite == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            this.OnWrite(buffer, offset, count);
+        }
+    }
+}
---
+++ src/TextFx.Tests/PushbackInputStreamTests.cs
@@ -0,0 +1,53 @@
+﻿namespace TextFx.Tests
+{
+    using Xunit;
+
+    public class PushbackInputStreamTests
+    {
+        [Fact]
+        public void CanWrite_ReturnsTrueIfSeekingIsNotSupported()
+        {
+            using (var stub = new FakeStream { OnCanReadGet = () => true, OnCanSeekGet = () => false })
+            using (var pushbackStream = new PushbackInputStream(stub))
+            {
+                Assert.True(pushbackStream.CanWrite);
+            }
+        }
+
+        [Fact]
+        public void CanWrite_ReturnsFalseIfSeekingIsSupported()
+        {
+            using (var stub = new FakeStream { OnCanReadGet = () => true, OnCanSeekGet = () => true })
+            using (var pushbackStream = new PushbackInputStream(stub))
+            {
+                Assert.False(pushbackStream.CanWrite);
+            }
+        }
+
+        [Fact]
+        public void Ctor_SetsInitialPositionSameAsUnderlyingStream()
+        {
+            const int Position = 123;
+            using (var stub = new FakeStream { OnCanReadGet = () => true, OnPositionGet = () => Position })
+            using (var pushbackStream = new PushbackInputStream(stub))
+            {
+                Assert.Equal(Position, pushbackStream.Position);
+            }
+        }
+
+        [Fact(Skip = "https://github.com/StevenLiekens/TextFx/issues/5")]
+        public void Write_SetsNewPosition()
+        {
+            const int Position = 128;
+            const int PushbackSize = 8;
+            const int ExpectedPosition = Position - PushbackSize;
+            using (var stub = new FakeStream { OnCanReadGet = () => true, OnCanSeekGet = () => false, OnPositionGet = () => Position})
+            using (var pushbackStream = new PushbackInputStream(stub))
+            {
+                var pushbackBytes = new byte[PushbackSize];
+                pushbackStream.Write(pushbackBytes, 0, PushbackSize);
+                Assert.Equal(ExpectedPosition, pushbackStream.Position);
+            }
+        }
+    }
+}
