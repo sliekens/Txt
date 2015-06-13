ec3c71451b3947778ad5a791128fa6acbaaebb78
Steven Liekens <steven.liekens@gmail.com>
2015-06-13 15:47:08 +0200

    Removing an invalid test case (see #5)

--- src/TextFx.Tests/PushbackInputStreamTests.cs
+++ src/TextFx.Tests/PushbackInputStreamTests.cs
@@ -67,27 +67,6 @@
             }
         }
 
-        [Fact(Skip = "https://github.com/StevenLiekens/TextFx/issues/5")]
-        public void Write_SetsNewPosition()
-        {
-            const int Position = 128;
-            const int PushbackSize = 8;
-            const int ExpectedPosition = Position - PushbackSize;
-            using (
-                var stub = new FakeStream
-                               {
-                                   OnCanReadGet = () => true,
-                                   OnCanSeekGet = () => false,
-                                   OnPositionGet = () => Position
-                               })
-            using (var pushbackStream = new PushbackInputStream(stub))
-            {
-                var pushbackBytes = new byte[PushbackSize];
-                pushbackStream.Write(pushbackBytes, 0, PushbackSize);
-                Assert.Equal(ExpectedPosition, pushbackStream.Position);
-            }
-        }
-
         [Fact]
         public void Write_SeekableStreamThrowsInvalidOperationException()
         {
