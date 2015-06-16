4e58965db22dce17b30c07f20482e213dde11e1c
Steven Liekens <steven.liekens@gmail.com>
2015-06-16 17:22:39 +0200

    Adding FakeLexer

---
+++ src/TextFx.Tests/FakeLexer.cs
@@ -0,0 +1,60 @@
+﻿namespace TextFx.Tests
+{
+    using System;
+
+    public class FakeLexer<T> : ILexer<T>
+        where T : Element
+    {
+        public delegate bool FakeTryRead(ITextScanner scanner, out T element);
+
+        public delegate bool FakeTryReadElement(ITextScanner scanner, out Element element);
+
+        public Func<ITextScanner, T> OnRead { get; set; }
+
+        public Func<ITextScanner, Element> OnReadElement { get; set; }
+
+        public FakeTryRead OnTryRead { get; set; }
+
+        public FakeTryReadElement OnTryReadElement { get; set; }
+
+        public T Read(ITextScanner scanner)
+        {
+            if (this.OnRead == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            return this.OnRead(scanner);
+        }
+
+        public Element ReadElement(ITextScanner scanner)
+        {
+            if (this.OnReadElement == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            return this.OnReadElement(scanner);
+        }
+
+        public bool TryRead(ITextScanner scanner, out T element)
+        {
+            if (this.OnTryRead == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            return this.OnTryRead(scanner, out element);
+        }
+
+        public bool TryReadElement(ITextScanner scanner, out Element element)
+        {
+            if (this.OnTryReadElement == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            return this.OnTryReadElement(scanner, out element);
+        }
+    }
+}
