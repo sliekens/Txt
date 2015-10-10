1b7b574021024fab7c966b37fcd245b04ede9fc1
Steven Liekens <steven.liekens@gmail.com>
2015-10-10 11:27:06 +0200

    Add ReadResult and SyntaxError classes

---
+++ src/TextFx/ReadResult.cs
@@ -0,0 +1,30 @@
+﻿namespace TextFx
+{
+    public class ReadResult<T>
+        where T : Element
+    {
+        public SyntaxError Error { get; set; }
+
+        public T Element { get; set; }
+
+        public bool Success { get; set; }
+
+        public static ReadResult<T> FromResult(T result)
+        {
+            return new ReadResult<T>
+            {
+                Success = true,
+                Element = result
+            };
+        }
+
+        public static ReadResult<T> FromError(SyntaxError error)
+        {
+            return new ReadResult<T>
+            {
+                Success = false,
+                Error = error
+            };
+        }
+    }
+}
---
+++ src/TextFx/SyntaxError.cs
@@ -0,0 +1,13 @@
+﻿namespace TextFx
+{
+    public class SyntaxError
+    {
+        public SyntaxError Details { get; set; }
+
+        public string RuleName { get; set; }
+
+        public string Message { get; set; }
+
+        public ITextContext Context { get; set; }
+    }
+}
