2e441ad4b393265d65ed61105bb5e809543a3cc6
Steven Liekens <steven.liekens@gmail.com>
2015-10-17 12:50:56 +0200

    Add tests

---
+++ src/TextFx.Tests/FakeTextSource.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+
+namespace TextFx.Tests
+{
+    public class FakeTextSource : ITextSource
+    {
+        public Action OnDispose { get; set; }
+
+        public Func<int> OnRead { get; set; }
+
+        public Action<char> OnUnreadChar { get; set; }
+
+        public Func<char[], int, int, int> OnReadCharArrayInt32Int32 { get; set; }
+
+        public Action<char[], int, int> OnUnreadCharArrayInt32Int32 { get; set; }
+
+        public Func<Encoding> OnEncodingGet { get; set; }
+
+        public void Dispose()
+        {
+            if (OnDispose == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            OnDispose();
+        }
+
+        public int Read()
+        {
+            if (OnRead == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            return OnRead();
+        }
+
+        public void Unread(char c)
+        {
+            if (OnUnreadChar == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            OnUnreadChar(c);
+        }
+
+        public int Read(char[] buffer, int index, int count)
+        {
+            if (OnReadCharArrayInt32Int32 == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            return OnReadCharArrayInt32Int32(buffer, index, count);
+        }
+
+        public void Unread(char[] buffer, int index, int count)
+        {
+            if (OnUnreadCharArrayInt32Int32 == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            OnUnreadCharArrayInt32Int32(buffer, index, count);
+        }
+
+        public Encoding Encoding
+        {
+            get
+            {
+                if (OnEncodingGet == null)
+                {
+                    throw new NotImplementedException();
+                }
+
+                return OnEncodingGet();
+            }
+        }
+    }
+}
---
+++ src/TextFx.Tests/TextScannerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+
+namespace TextFx.Tests
+{
+    public class TextScannerTests
+    {
+        [Fact]
+        public void Ctor_ThrowsOnNullAsTextSource()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TextScanner(null));
+        }
+
+        [Fact]
+        public void Ctor_DoesNotInitializeReader()
+        {
+            var stub = new FakeTextSource();
+            var sut = new TextScanner(stub);
+            Assert.Equal(-1, sut.Offset);
+        }
+
+        [Fact]
+        public void TryMatch_ThrowsWhenNotInitialized()
+        {
+            var stub = new FakeTextSource();
+            var sut = new TextScanner(stub);
+            char next;
+            Assert.Throws<InvalidOperationException>(() => sut.TryMatch('x', out next));
+        }
+
+        [Fact]
+        public void TryMatchIgnoreCase_ThrowsWhenNotInitialized()
+        {
+            var stub = new FakeTextSource();
+            var sut = new TextScanner(stub);
+            char next;
+            Assert.Throws<InvalidOperationException>(() => sut.TryMatchIgnoreCase('x', out next));
+        }
+
+        [Fact]
+        public void GetContext_ThrowsWhenNotInitialized()
+        {
+            var stub = new FakeTextSource();
+            var sut = new TextScanner(stub);
+            Assert.Throws<InvalidOperationException>(() => sut.GetContext());
+        }
+    }
+}
