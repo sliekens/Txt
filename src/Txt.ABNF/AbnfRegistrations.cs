1a25d376f74f3cf7e401498dccd4f8100a4ec307
Steven Liekens <steven.liekens@gmail.com>
2016-04-23 04:26:24 -0700

    Improve registrations

---
+++ src/Txt.ABNF/AbnfRegistrations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Txt.ABNF
+{
+    public sealed class AbnfRegistrations : Registrations
+    {
+        public static IEnumerable<Registration> GetRegistrations(GetInstanceDelegate getInstance)
+        {
+            if (getInstance == null)
+            {
+                throw new ArgumentNullException(nameof(getInstance));
+            }
+            yield return new Registration(typeof(ITerminalLexerFactory), typeof(TerminalLexerFactory));
+            yield return new Registration(typeof(IValueRangeLexerFactory), typeof(ValueRangeLexerFactory));
+            yield return new Registration(typeof(IAlternationLexerFactory), typeof(AlternationLexerFactory));
+            yield return new Registration(typeof(IGreedyAlternationLexerFactory), typeof(GreedyAlternationLexerFactory));
+            yield return new Registration(typeof(IConcatenationLexerFactory), typeof(ConcatenationLexerFactory));
+            yield return new Registration(typeof(IRepetitionLexerFactory), typeof(RepetitionLexerFactory));
+            yield return new Registration(typeof(IOptionLexerFactory), typeof(OptionLexerFactory));
+            foreach (var registration in GetRegistrations(typeof(AbnfRegistrations).GetTypeInfo().Assembly, getInstance))
+            {
+                yield return registration;
+            }
+        }
+    }
+}
---
+++ test/Txt.ABNF.Tests/AbnfRegistrationsTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using SimpleInjector;
+using Xunit;
+
+namespace Txt.ABNF
+{
+    public class AbnfRegistrationsTest
+    {
+        private readonly Container container = new Container();
+
+        [Fact]
+        public void Test()
+        {
+            foreach (var registration in AbnfRegistrations.GetRegistrations(GetInstance))
+            {
+                if (registration.Implementation != null)
+                {
+                    container.RegisterSingleton(registration.Service, registration.Implementation);
+                }
+                if (registration.Instance != null)
+                {
+                    container.RegisterSingleton(registration.Service, registration.Instance);
+                }
+                if (registration.Factory != null)
+                {
+                    container.RegisterSingleton(registration.Service, registration.Factory);
+                }
+            }
+            container.Verify();
+        }
+
+        private object GetInstance(Type service)
+        {
+            return container.GetInstance(service);
+        }
+
+        private void Register(Type service, Type implementation)
+        {
+            container.Register(service, implementation, Lifestyle.Singleton);
+        }
+
+        private void RegisterFactory(Type service, Func<object> implementationFactory)
+        {
+            container.Register(service, implementationFactory, Lifestyle.Singleton);
+        }
+    }
+}
