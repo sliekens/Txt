68b6aa7a2d25eef1b406201c83689d39760f3e96
Steven Liekens <steven.liekens@gmail.com>
2016-05-13 20:10:52 +0200

    Fix annotations namespace

--- src/Common/compiler/shared/JetBrains.Annotations.cs
+++ src/Common/compiler/shared/JetBrains.Annotations.cs
@@ -9,7 +9,7 @@ using System;
 // ReSharper disable MemberCanBeProtected.Global
 // ReSharper disable InconsistentNaming
 
-namespace Jetbrains.Annotations
+namespace JetBrains.Annotations
 {
     /// <summary>
     ///     Indicates that the value of the marked element could be <c>null</c> sometimes,
