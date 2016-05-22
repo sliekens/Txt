42c6408990207d4f47d1495bcb0e2b9b2fb7e89e
Steven Liekens <steven.liekens@gmail.com>
2016-05-22 14:40:39 +0200

    Add DoubleQuoteParser

---
+++ src/Txt.ABNF/Core/DQUOTE/DoubleQuoteParser.cs
@@ -0,0 +1,12 @@
+﻿using Txt.Core;
+
+namespace Txt.ABNF.Core.DQUOTE
+{
+    public class DoubleQuoteParser : Parser<DoubleQuote, char>
+    {
+        protected override char ParseImpl(DoubleQuote value)
+        {
+            return value.Text[0];
+        }
+    }
+}
