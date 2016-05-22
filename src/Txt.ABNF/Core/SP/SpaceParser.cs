98062611c2aea30531bbe0334c092aeb09245cf0
Steven Liekens <steven.liekens@gmail.com>
2016-05-22 15:32:43 +0200

    Add SpaceParser

---
+++ src/Txt.ABNF/Core/SP/SpaceParser.cs
@@ -0,0 +1,12 @@
+﻿using Txt.Core;
+
+namespace Txt.ABNF.Core.SP
+{
+    public class SpaceParser : Parser<Space, char>
+    {
+        protected override char ParseImpl(Space value)
+        {
+            return value.Text[0];
+        }
+    }
+}
