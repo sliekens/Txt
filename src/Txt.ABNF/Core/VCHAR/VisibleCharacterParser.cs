194f4624887c2bbdcf746d310258513f416f5e76
Steven Liekens <steven.liekens@gmail.com>
2016-05-22 15:33:12 +0200

    Add VisibleCharacterParser

---
+++ src/Txt.ABNF/Core/VCHAR/VisibleCharacterParser.cs
@@ -0,0 +1,12 @@
+﻿using Txt.Core;
+
+namespace Txt.ABNF.Core.VCHAR
+{
+    public class VisibleCharacterParser : Parser<VisibleCharacter, char>
+    {
+        protected override char ParseImpl(VisibleCharacter value)
+        {
+            return value.Text[0];
+        }
+    }
+}
