8fa776ca1cd16965d5895e4d5724b3a318040f6c
Steven Liekens <steven.liekens@gmail.com>
2016-05-22 14:32:57 +0200

    Add CarriageReturnParser

---
+++ src/Txt.ABNF/Core/CR/CarriageReturnParser.cs
@@ -0,0 +1,12 @@
+﻿using Txt.Core;
+
+namespace Txt.ABNF.Core.CR
+{
+    public class CarriageReturnParser : Parser<CarriageReturn, char>
+    {
+        protected override char ParseImpl(CarriageReturn value)
+        {
+            return value.Text[0];
+        }
+    }
+}
