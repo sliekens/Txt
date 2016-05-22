4d1704a33c06340c68c26e1967f95465048483dc
Steven Liekens <steven.liekens@gmail.com>
2016-05-22 13:54:43 +0200

    Add IParser

---
+++ src/Txt.Core/IParser.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace Txt.Core
+{
+    public interface IParser<in TElement, out TResult>
+        where TElement : Element
+    {
+        [NotNull]
+        TResult Parse([NotNull] TElement value);
+    }
+}
