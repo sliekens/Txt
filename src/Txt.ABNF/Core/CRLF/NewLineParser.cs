42b9ab145c19fbe9ce4937785b0425c8552b86a5
Steven Liekens <steven.liekens@gmail.com>
2016-05-22 14:34:28 +0200

    Add NewLineParser

---
+++ src/Txt.ABNF/Core/CRLF/NewLineParser.cs
@@ -0,0 +1,12 @@
+﻿using Txt.Core;
+
+namespace Txt.ABNF.Core.CRLF
+{
+    public class NewLineParser : Parser<NewLine, string>
+    {
+        protected override string ParseImpl(NewLine value)
+        {
+            return value.Text;
+        }
+    }
+}
