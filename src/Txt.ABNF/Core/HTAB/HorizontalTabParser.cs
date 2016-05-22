c8a9e414235b19c65dbbeac5be371c2c2f7065a0
Steven Liekens <steven.liekens@gmail.com>
2016-05-22 14:45:16 +0200

    Add HorizontalTabParser

---
+++ src/Txt.ABNF/Core/HTAB/HorizontalTabParser.cs
@@ -0,0 +1,12 @@
+﻿using Txt.Core;
+
+namespace Txt.ABNF.Core.HTAB
+{
+    public class HorizontalTabParser : Parser<HorizontalTab, char>
+    {
+        protected override char ParseImpl(HorizontalTab value)
+        {
+            return value.Text[0];
+        }
+    }
+}
