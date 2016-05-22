068557461b5eaf6d1d7566d187d6b8817e0432b5
Steven Liekens <steven.liekens@gmail.com>
2016-05-22 14:45:52 +0200

    Add LineFeedParser

---
+++ src/Txt.ABNF/Core/LF/LineFeedParser.cs
@@ -0,0 +1,12 @@
+﻿using Txt.Core;
+
+namespace Txt.ABNF.Core.LF
+{
+    public class LineFeedParser : Parser<LineFeed, char>
+    {
+        protected override char ParseImpl(LineFeed value)
+        {
+            return value.Text[0];
+        }
+    }
+}
