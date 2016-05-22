c43259e2eee2f1bbefcf75956b58f5f6a6e3de08
Steven Liekens <steven.liekens@gmail.com>
2016-05-22 14:35:06 +0200

    Add ControlCharacterParser

---
+++ src/Txt.ABNF/Core/CTL/ControlCharacterParser.cs
@@ -0,0 +1,12 @@
+﻿using Txt.Core;
+
+namespace Txt.ABNF.Core.CTL
+{
+    public class ControlCharacterParser : Parser<ControlCharacter, char>
+    {
+        protected override char ParseImpl(ControlCharacter value)
+        {
+            return value.Text[0];
+        }
+    }
+}
