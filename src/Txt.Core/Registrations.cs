125faf91988d034967a2b7c366643cb24c78295f
Steven Liekens <steven.liekens@gmail.com>
2016-06-26 01:58:00 +0200

    Add parsers to Registrations

--- src/Txt.Core/Registrations.cs
+++ src/Txt.Core/Registrations.cs
@@ -25,33 +25,49 @@ namespace Txt.Core
             var genericLexerType = typeof(ILexer<>);
             var genericLexerFactoryType = typeof(ILexerFactory<>).GetTypeInfo();
             var exportedTypes = assembly.ExportedTypes;
-            var registrations = (from type in exportedTypes
-                                 let typeInfo = type.GetTypeInfo()
-                                 where !typeInfo.IsAbstract
-                                 let lexerFactoryType =
-                                     typeInfo.ImplementedInterfaces.SingleOrDefault(
-                                         o =>
-                                             o.IsConstructedGenericType &&
-                                             Equals(o.GetGenericTypeDefinition().GetTypeInfo(), genericLexerFactoryType))
-                                 where lexerFactoryType != null
-                                 let factoryMethod = lexerFactoryType.GetTypeInfo().DeclaredMethods.Single()
-                                 let lexerType =
-                                     genericLexerType.MakeGenericType(lexerFactoryType.GenericTypeArguments[0])
-                                 select
-                                     new
-                                     {
-                                         FactoryService = lexerFactoryType,
-                                         FactoryImplementation = type,
-                                         LexerService = lexerType,
-                                         FactoryMethod = factoryMethod
-                                     }).ToList();
-            foreach (var registration in registrations)
+            var lexers = (from type in exportedTypes
+                          let typeInfo = type.GetTypeInfo()
+                          where !typeInfo.IsAbstract
+                          let lexerFactoryType =
+                              typeInfo.ImplementedInterfaces.SingleOrDefault(
+                                  o =>
+                                      o.IsConstructedGenericType &&
+                                      Equals(o.GetGenericTypeDefinition().GetTypeInfo(), genericLexerFactoryType))
+                          where lexerFactoryType != null
+                          let factoryMethod = lexerFactoryType.GetTypeInfo().DeclaredMethods.Single()
+                          let lexerType =
+                              genericLexerType.MakeGenericType(lexerFactoryType.GenericTypeArguments[0])
+                          select
+                              new
+                              {
+                                  FactoryService = lexerFactoryType,
+                                  FactoryImplementation = type,
+                                  LexerService = lexerType,
+                                  FactoryMethod = factoryMethod
+                              }).ToList();
+            var parsers = from type in exportedTypes
+                          let typeInfo = type.GetTypeInfo()
+                          where !typeInfo.IsAbstract
+                          let service =
+                          typeInfo.ImplementedInterfaces.SingleOrDefault(
+                                      o => o.IsConstructedGenericType && (o.GetGenericTypeDefinition() == typeof(IParser<,>)))
+                          where service != null
+                          select new
+                          {
+                              Service = service,
+                              Implementation = type
+                          };
+            foreach (var registration in lexers)
             {
                 yield return new Registration(registration.FactoryService, registration.FactoryImplementation);
                 yield return
                     new Registration(
                         registration.LexerService,
                         () => registration.FactoryMethod.Invoke(getInstance(registration.FactoryService), null));
+            }
+            foreach (var registration in parsers)
+            {
+                yield return new Registration(registration.Service, registration.Implementation);
             }
         }
     }
