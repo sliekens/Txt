03a816f19d5e31b99a718646a18852d2785c1f32
Steven Liekens <steven.liekens@gmail.com>
2016-09-02 17:01:10 +0200

    Add Peek tests

---
+++ test/Txt.Core.Tests/TextSourceTests.Peek.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace Txt.Core
+{
+    public partial class TextSourceTests
+    {
+        public class Peek
+        {
+            [Fact]
+            public void EmptyData()
+            {
+                var sut = new StringTextSource("");
+                Assert.Equal(-1, sut.Peek());
+                Assert.Equal(0, sut.Offset);
+                Assert.Equal(1, sut.Line);
+                Assert.Equal(1, sut.Column);
+            }
+
+            [Theory]
+            [InlineData("0")]
+            [InlineData("9")]
+            [InlineData("!")]
+            [InlineData("A")]
+            [InlineData("Z")]
+            [InlineData("ù")]
+            [InlineData("☕")]
+            [InlineData("\0")]
+            [InlineData("\uFFFF")]
+            public void NonEmptyData(string data)
+            {
+                var sut = new StringTextSource(data);
+                Assert.Equal(data, char.ToString((char)sut.Peek()));
+                Assert.Equal(0, sut.Offset);
+                Assert.Equal(1, sut.Line);
+                Assert.Equal(1, sut.Column);
+            }
+        }
+    }
+}
