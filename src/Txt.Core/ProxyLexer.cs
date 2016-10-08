c6fc2758b39fb06de97d2b74649a6650865562fb
Steven Liekens <steven.liekens@gmail.com>
2016-10-08 14:32:19 +0200

    Add ProxyLexer.Initialized : bool

--- src/Txt.Core/ProxyLexer.cs
+++ src/Txt.Core/ProxyLexer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using JetBrains.Annotations;
 
 namespace Txt.Core
@@ -9,26 +10,30 @@ namespace Txt.Core
     {
         private ILexer<TElement> innerLexer;
 
+        public bool Initialized { get; private set; }
+
         public void Initialize([NotNull] ILexer<TElement> lexer)
         {
             if (lexer == null)
             {
                 throw new ArgumentNullException(nameof(lexer));
             }
-            if (innerLexer != null)
+            if (Initialized)
             {
                 throw new InvalidOperationException(
                     "Initialize(ILexer`1) has already been called. Changing the rule after it has been initialized is not allowed.");
             }
             innerLexer = lexer;
+            Initialized = true;
         }
 
         protected override IEnumerable<TElement> ReadImpl(ITextScanner scanner, ITextContext context)
         {
-            if (innerLexer == null)
+            if (!Initialized)
             {
                 throw new InvalidOperationException("Initialize(ILexer`1) has never been called.");
             }
+            Debug.Assert(innerLexer != null, "innerLexer != null");
             return innerLexer.Read(scanner, context);
         }
     }
